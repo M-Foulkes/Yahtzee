8c4c2f017d97a07094228164acc0b100557a98a9
HiltonBros <45970023+HiltonBros@users.noreply.github.com>
2019-01-04 07:53:38 -0500

    Update Bot.java

--- Bot.java
+++ Bot.java
@@ -4,7 +4,7 @@ public class Bot
 {
     private static DiceArray a = new DiceArray(5);
     private static int dice[];
-    private static Scoreboard s = new Scoreboard();
+    private static Upper_Scoreboard s = new Upper_Scoreboard();
     private static int ones;
     private static int twos;
     private static int threes;
@@ -18,8 +18,7 @@ public class Bot
     private static int fh;
     private static int chance;
     private static int yahtzee;
-    private static String downs;
-    private static int ups;
+    private static String c;
     private static int value;
     private static String name;
     
@@ -36,104 +35,95 @@ public class Bot
     
     public void highestChoice()
     {
-        ones = s.getValues(dice[], 1);
-        twos = s.getValues(dice[], 2);
-        threes = s.getValues(dice[], 3);
-        fours = s.getValues(dice[], 4);
-        fives = s.getValues(dice[], 5);
-        sixes = s.getValues(dice[],  6);
-        toak = s.getValues(dice[], a);
-        foak = s.getValues(dice[], b);
-        ss = s.getValues(dice[], c);
-        ls = s.getValues(dice[], d);
-        fh = s.getValues(dice[], e);
-        chance = s.getValues(dice[], f);
-        yahtzee = s.getValues(dice[], g);
+        ones = s.getValues(dice[], "1");
+        twos = s.getValues(dice[], "2");
+        threes = s.getValues(dice[], "3");
+        fours = s.getValues(dice[], "4");
+        fives = s.getValues(dice[], "5");
+        sixes = s.getValues(dice[], "6");
+        toak = s.getValues(dice[], "a");
+        foak = s.getValues(dice[], "b");
+        ss = s.getValues(dice[], "c");
+        ls = s.getValues(dice[], "d");
+        fh = s.getValues(dice[], "e");
+        chance = s.getValues(dice[], "f");
+        yahtzee = s.getValues(dice[], "g");
         int[] choice = {ones, twos, threes, fours, fives, sixes, toak, foak, s, ls, fh, chance, yahtzee};
         Arrays.sort(choice);
         value = choice[12];
         if (value == ones)
         {
-            ups = 1;
+            c = "1";
             name = "Ones";
         }
         if (value == twos)
         {
-            ups = 2;
+            c = "2";
             name = "Twos";
         }
         if (value == threes)
         {
-            ups = 3;
+            c = "3";
             name = "Threes";
         }
         if (value == fours)
         {
-            ups = 4;
+            c = "4";
             name = "Fours";
         }
         if (value == fives)
         {
-            ups = 5;
+            c = "5";
             name = "Fives";
         }
         if (value == sixes)
         {
-            ups = 6;
+            c = "6";
             name = "Sixes";
         }
         if (value == toak)
         {
-            downs = "a";
+            c = "a";
             name = "Three of a Kind";
         }
         if (value == foak)
         {
-            downs = "b";
+            c = "b";
             name = "Four of a Kind";
         }
         if (value == ss)
         {
-            downs = "c";
+            c = "c";
             name = "Short Straight";
         }
         if (value == ls)
         {
-            downs = "d";
+            c = "d";
             name = "Long Straight";
         }
         if (value == fh)
         {
-            downs = "e";
+            c = "e";
             name = "Full House";
         }
         if (value == chance)
         {
-            downs = "f";
+            c = "f";
             name = "Chance";
         }
         if (value == yahtzee)
         {
-            downs = "g";
+            c = "g";
             name = "Yahtzee";
         }
-            
-        if(downs != null)
-        {
-            s.setScore(value, false, downs);
-        }
-        else
-        {
-            s.setScore(value, false, ups);
-        }
+      
+        s.setScore(value, false, c);
     }
     
     public String toString()
     {
-        if(downs != null)
-            return "The bot chose " + name + " for " + downs + " points";
-        else
-            return "The bot chose " + name + " for " + ups + " points";
+        
+        return "The bot chose " + name + " for " + c + " points";
     }
   
 }
