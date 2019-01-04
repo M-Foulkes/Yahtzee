cf34e2009fa23869003a1a50401bf47bfb738ad7
KennyCullum <45969926+KennyCullum@users.noreply.github.com>
2019-01-04 07:41:16 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class MyProgram
+public class Player
 {
     public static String keep;
     public static void main(String[] args)
@@ -8,13 +8,13 @@ public class MyProgram
         int turn = 1;
         String kept = "You kept: ";
         Scanner at = new Scanner(System.in);
-        int[] dice = MyProgram.getDice();
+        DiceArray dice = new DiceArray(5);
         //dice[0] = (int)(Math.random()*6 + 1);
         //dice[1] = (int)(Math.random()*6 + 1);
         //dice[2] = (int)(Math.random()*6 + 1);
         //dice[3] = (int)(Math.random()*6 + 1);
         //dice[4] = (int)(Math.random()*6 + 1);
-        System.out.print(dice[0]);
+        //System.out.print(dice[0]);
         while(turn <= 3)
         {
             if(turn < 3)
@@ -24,39 +24,39 @@ public class MyProgram
                     keep = keep.toUpperCase();
                     if(keep.equals("N"))
                     {
-                        System.out.print("In order of dice choose to Keep or Re-roll (K/R) ");
+                    System.out.print("In order of dice choose to Keep or Re-roll (K/R) ");
                     String kr = at.nextLine();
                     kr = kr.toUpperCase();
                     if(kr.substring(0,1).equals("K"))
                     {
                     kept += ("Die 1: " + dice[0] + " "); 
                     }
-                    //else
-                        //Call re-roll
+                    else
+                        dice[0] = dice.roll();
                     if(kr.substring(1,2).equals("K"))
                     {
                         kept += ("Die 2: " + dice[1] + " ");
                     }
-                    //else
-                        //Call re-roll
+                    else
+                        dice[1] = dice.roll();
                     if(kr.substring(2,3).equals("K"))
                     {
                         kept += ("Die 3: " + dice[2] + " ");
                     }
-                    //else
-                        //Call re-roll
+                    else
+                        dice[2] = dice.roll();
                     if(kr.substring(3,4).equals("K"))
                     {
                         kept += ("Die 4: " + dice[3] + " ");
                     }
-                    //else
-                        //Call re-roll
+                    else
+                        dice[3] = dice.roll();
                     if(kr.substring(4,5).equals("K"))
                     {
                         kept += ("Die 5: " + dice[4] + " ");
                     }
-                    //else
-                        //Call re-roll
+                    else
+                        dice[4] = dice.roll();
                     System.out.println(kept);
                     kept = "You Kept: ";
                     turn++;
